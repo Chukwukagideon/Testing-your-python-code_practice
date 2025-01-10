6e34bd9d63cb87dbe20b6327f7eddbc8ca509680
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-01-10 18:12:50 +0100

    added a test to test for scenarios where population is given

--- test_cities.py
+++ test_cities.py
@@ -1,9 +1,15 @@
 import unittest
 from city_functions import get_city_country
 
+
 class CitiesTestCase(unittest.TestCase):
     """Test for city_functions.py"""
     def test_city_country(self):
-        """Do names like Santiago, Chile? """
+        """Do names like Santiago, Chile work? """
         formatted_name = get_city_country("santiago", "chile")
         self.assertEqual(formatted_name, "Santiago, Chile")
+
+    def test_city_country_population(self):
+        """Will parameters like Santiago, Chile and population-500000 work?"""
+        formatted_name = get_city_country("santiago", "chile", "population-500000")
+        self.assertEqual(formatted_name, "Santiago, Chile - Population-500000.")
