c15b6b834c7c6c85c6af18d00ab66f065151e832
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-01-10 19:39:22 +0100

    updated code added a method to test if the give_raise method of the Employee class would work for giving custom raise

--- test_employee.py
+++ test_employee.py
@@ -9,9 +9,16 @@ class TestEmployee(unittest.TestCase):
         self.my_employee = Employee("John", "Doe", 40000)
         self.my_employee.give_raise()
 
+        self.my_employee2 = Employee("John", "Doe", 40000)
+        self.my_employee2.give_raise(6000)
+
     def test_give_default_raise(self):
         """Test if the default raise amount works"""
         self.assertEqual(self.my_employee.annual_salary, 45000)
+
+    def test_give_custom_raise(self):
+        """Test if giving a custom raise works"""
+        self.assertEqual(self.my_employee2.annual_salary, 46000)
 
 
 if __name__ == "__main__":
