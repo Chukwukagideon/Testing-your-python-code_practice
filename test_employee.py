5863dbac3b1111aeebca79ee234221e98b0a64f7
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-01-10 19:26:27 +0100

    updated code created a test class with one method to test if the default raise is applied correctly

--- test_employee.py
+++ test_employee.py
@@ -1,3 +1,18 @@
 import unittest
 from employee import Employee
 
+
+class TestEmployee(unittest.TestCase):
+    """Tests for the employee class"""
+
+    def setUp(self):
+        self.my_employee = Employee("John", "Doe", 40000)
+        self.my_employee.give_raise()
+
+    def test_give_default_raise(self):
+        """Test if the default raise amount works"""
+        self.assertEqual(self.my_employee.annual_salary, 45000)
+
+
+if __name__ == "__main__":
+    unittest.main()
